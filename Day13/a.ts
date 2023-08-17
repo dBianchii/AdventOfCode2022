f4b07593e81c1c846371c817b23d2901dae9f1de
Gabriel Bianchi <gdbianchii@gmail.com>
2023-08-17 14:50:25 -0300

    Day 13 - a

---
+++ Day13/a.ts
@@ -0,0 +1,71 @@
+import { readFileSync } from "fs";
+
+const input = readFileSync("./Day13/input.txt", "utf-8");
+
+let lines = input.split("\n");
+
+let correctOrderIndexes = new Array<number>();
+let correctOrder = false
+let finished = false;
+let currentLine = 1;
+lines.forEach((line, i) => {
+	if (i % 3 !== 0) return;
+ 
+  const left = JSON.parse(line) as Array<number>;
+  const right = JSON.parse(lines[i + 1]) as Array<number>;
+
+	correctOrder = false;
+	finished = false;
+
+	Compare(left, right)
+	if (correctOrder)
+		correctOrderIndexes.push(currentLine);
+
+	correctOrder = false;
+	finished = false;
+	currentLine++;
+})
+//sum of all correct order indexes
+console.log(
+	correctOrderIndexes.reduce((acc, curr) => acc + curr, 0)
+);
+
+function Compare(a: number | number[], b: number | number[]): void {
+	if (finished) return;
+
+	if (typeof a === "number" && typeof b === "number"){
+		if (a < b){
+			correctOrder = true;
+			finished = true
+		}
+		else if (a > b) finished = true;
+
+		return
+	}
+
+	if (Array.isArray(a) && Array.isArray(b)) {
+		const biggestArrayLength = a.length > b.length ? a.length : b.length;
+		for (let i = 0; i < biggestArrayLength; i++) {
+			if (finished) return;
+
+			if (a[i] === undefined) {
+				correctOrder = true
+				finished = true
+				return;
+			};
+
+			if (b[i] === undefined) {
+				finished = true
+				return;
+			};
+			
+      Compare(a[i], b[i]);
+    }
+		return
+	}
+
+	if (typeof a === "number") a = [a];
+	if (typeof b === "number") b = [b];
+	
+	Compare(a, b);
+}
